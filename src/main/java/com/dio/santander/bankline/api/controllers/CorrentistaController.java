397181f89add10c48074579a9346d09a2cf0ebc4
Naiara Barros <naiara.b.s@outlook.com>
2022-05-11 15:15:08 -0300

    Update CorrentistaController.java

--- src/main/java/com/dio/santander/bankline/api/controllers/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controllers/CorrentistaController.java
@@ -1,5 +1,23 @@
 package com.dio.santander.bankline.api.controllers;
 
-public class CorrentistaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+
+@RestControllerAdvice
+@RequestMapping("/correntistas")
+public class CorrentistaController {
+	@Autowired
+	private CorrentistaRepository repository;
+	
+	@GetMapping
+	public List<Correntista> findAll(){
+		return repository.findAll();
+	}
 }
